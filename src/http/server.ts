66fb387e2b99537eff6dd30afc7988ab6d2bb0a9
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-14 17:33:58 -0300

    feat: add zod

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,24 @@
 import fastify from 'fastify'
+import { z } from 'zod'
 
 const app = fastify()
+
+
+app.post('/polls', async (request, response) => {
+  const createPollBodySchema = z.object({
+    title: z.string(),
+  })
+
+  const { title } = createPollBodySchema.parse(request.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    }
+  })
+
+  return response.status(201).send({ pollId: poll.id })
+})
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server running')
