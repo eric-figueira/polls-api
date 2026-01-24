36e08403e04a22c9b93047a72502f4e1231b45d6
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-24 11:52:57 -0300

    feat: create get poll route

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,29 @@
+import type { FastifyInstance } from 'fastify'
+import z from 'zod'
+import { prisma } from '../../lib/prisma.js'
+
+export async function getPoll(app: FastifyInstance) {
+  app.get('/polls/:pollId', async (request, response) => {
+    const getPollParams = z.object({
+      pollId: z.uuid()
+    })
+
+    const { pollId } = getPollParams.parse(request.params)
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        id: pollId,
+      },
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true,
+          }
+        }
+      }
+    })
+
+    return response.send({ poll })
+  })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { createPoll } from './routes/create-poll.js'
+import { getPoll } from './routes/get-poll.js'
 
 const app = fastify()
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server running')
