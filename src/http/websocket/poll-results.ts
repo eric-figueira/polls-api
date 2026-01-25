e98f15f4ec3f056dd0ab5c43124fa58eff47d4c8
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-25 13:05:27 -0300

    style: fix code style

--- src/http/websocket/poll-results.ts
+++ src/http/websocket/poll-results.ts
@@ -1,6 +1,6 @@
-import type { FastifyInstance } from "fastify";
-import { voting } from "../../utils/voting-pub-sub.js";
-import z from "zod";
+import type { FastifyInstance } from 'fastify'
+import { voting } from '../../utils/voting-pub-sub.js'
+import z from 'zod'
 
 export async function pollResults(app: FastifyInstance) {
   app.get('/polls/:pollId/results', { websocket: true }, (connection, request) => {
